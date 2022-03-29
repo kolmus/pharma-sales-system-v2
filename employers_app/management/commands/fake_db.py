31fd916be2aaf26f586981a4e1020c2b8d1357c3
Łukasz Kolmus <lukasz.kolmus@gmail.com>
2022-03-29 16:53:36 +0200

    util for CalendarSupervisor, mod in script

--- employers_app/management/commands/fake_db.py
+++ employers_app/management/commands/fake_db.py
@@ -1,15 +1,15 @@
 from django.core.management.base import BaseCommand
-from employers_app.tests.utils import create_employers
+from employers_app.tests.utils import create_employers, create_calendar_visit
 
 
 class Command(BaseCommand):
     """creates fake db for developers
         new users -> 50
         new employers -> 50
-
+        new visits -> 14
     Args:
         BaseCommand: python manage.py fake_db
     """
-
     def handle(self, *args, **options):
         create_employers(count=50)
+        create_calendar_visit(count=14)

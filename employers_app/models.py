4998696882275bb36d7e501e80ec4150f79fc0c5
Łukasz Kolmus <lukasz.kolmus@gmail.com>
2022-03-29 13:35:00 +0200

    add Employee model

--- employers_app/models.py
+++ employers_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Employee(models.Model):
+    phone = models.IntegerField(verbose_name="Numer telefonu")
+    role = models.CharField(max_length=128, verbose_name='Stanowisko')
+    supervisor = models.ForeignKey('Employee', verbose_name='Przełożony', on_delete=models.SET_NULL, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True, verbose_name='Zatrudniony')
+    is_supervisor = models.BooleanField(default=False, verbose_name='Stanowisko kierownicze')
+    
+    @property
+    def name(self):
+        return "{} {}".format(self.user.first_name, self.user.last_name)
+    
+    def __str__(self):
+        return self.name
+

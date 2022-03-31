d813202162636d7a46e134f17fde022896ac28fd
Łukasz Kolmus <lukasz.kolmus@gmail.com>
2022-03-31 13:15:45 +0200

    migration

--- employers_app/models.py
+++ employers_app/models.py
@@ -11,7 +11,7 @@ class User(AbstractUser):
     username = None
     phone = models.BigIntegerField(verbose_name="Numer telefonu")
     role = models.CharField(max_length=128, verbose_name="Stanowisko")
-    supervisor = models.ForeignKey("Employee", verbose_name="Przełożony", on_delete=models.SET_NULL, null=True)
+    supervisor = models.ForeignKey('User', verbose_name="Przełożony", on_delete=models.SET_NULL, null=True)
     is_active = models.BooleanField(default=True, verbose_name="Zatrudniony")
     is_supervisor = models.BooleanField(default=False, verbose_name="Stanowisko kierownicze")
     
